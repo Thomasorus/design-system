520a387193653e453b3ba0c893491ff41ebef13e
Thomasorus <contact@thomasorus.com>
2021-06-27 20:32:06 +0200

    Removed several plugins

--- postcss.config.js
+++ postcss.config.js
@@ -4,11 +4,11 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('autoprefixer'),
-    postcssCustomProperties({ importFrom: 'src/css/config.css' }),
-    require('postcss-color-function'), // replaces color functions with rgba values
-    require('postcss-color-rgba-fallback'), // adds a hex value before every rgba value
-    require('pixrem'), // adds pixel fallback before every rem value
-    require('postcss-calc'),
+    // postcssCustomProperties({ importFrom: 'src/css/config.css' }),
+    // require('postcss-color-function'), // replaces color functions with rgba values
+    // require('postcss-color-rgba-fallback'), // adds a hex value before every rgba value
+    // require('pixrem'), // adds pixel fallback before every rem value
+    // require('postcss-calc'),
     require('postcss-copy')({ dest: 'assets' })
   ]
 }
