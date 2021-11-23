94a9e69c6f8aa3b7a37758a38d85e4674de715ff
Thomasorus <contact@thomasorus.com>
2021-11-24 00:34:56 +0100

    started animations

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,79 @@
-(()=>{var d=document.querySelectorAll("input[required]");d.forEach(t=>{let o=t.getAttribute("id"),n=document.querySelector(`label[for="${o}"`),r='<span class="required">&nbsp;(Required)</span>';n.insertAdjacentHTML("beforeend",r)});var m=document.querySelectorAll('input[aria-invalid="true"]');m.forEach(t=>{let o=t.getAttribute("id"),n=o+"-error",r=o+"-info";document.querySelector(`div[id="${n}"]`).removeAttribute("hidden");let l=t.getAttribute("aria-describedby");t.setAttribute("aria-describedby",l+" "+n)});var g=document.querySelectorAll('.collapse [data-trigger="true"]');g.forEach(t=>{let o=t.tagName,n=t.dataset,r="";Object.keys(n).map(e=>{e!=="trigger"&&(r+=` data-${e} `)}),t.innerHTML=`
-    <button aria-expanded="false" ${r}>${t.textContent}</button>`;let l=(e=>{let s=[];for(;e.nextElementSibling&&e.nextElementSibling.tagName!==o;)s.push(e.nextElementSibling),e=e.nextElementSibling;return s.forEach(u=>{u.parentNode.removeChild(u)}),s})(t),i=document.createElement("div");i.hidden=!0,i.setAttribute("class","flow accessibility-menu"),l.forEach(e=>{i.appendChild(e)}),t.parentNode.insertBefore(i,t.nextElementSibling);let a=t.querySelector("button");a.onclick=()=>{let e=a.getAttribute("aria-expanded")==="true";a.setAttribute("aria-expanded",!e),i.hidden=e}});function h(){let t=localStorage.getItem("theme");t=="light"&&(document.documentElement.setAttribute("theme","light"),localStorage.setItem("theme","light")),t=="dark"&&(document.documentElement.setAttribute("theme","dark"),localStorage.setItem("theme","dark"));let o=localStorage.getItem("fontsize"),n=document.querySelector("html"),r=window.getComputedStyle(n,null).getPropertyValue("font-size"),c=parseFloat(r);n.style.fontSize=o;let l=localStorage.getItem("fontstyle");document.querySelector("html").setAttribute("font-style",l)}(function(){h()})();})();
+(() => {
+  // src/js/index.js
+  var allRequired = document.querySelectorAll("input[required]");
+  allRequired.forEach((el) => {
+    const id = el.getAttribute("id");
+    const label = document.querySelector(`label[for="${id}"`);
+    const text = `<span class="required">&nbsp;(Required)</span>`;
+    label.insertAdjacentHTML("beforeend", text);
+  });
+  var allAriaInvalid = document.querySelectorAll(`input[aria-invalid="true"]`);
+  allAriaInvalid.forEach((el) => {
+    const id = el.getAttribute("id");
+    const errorId = id + "-error";
+    const infoID = id + "-info";
+    const errorMessage = document.querySelector(`div[id="${errorId}"]`);
+    errorMessage.removeAttribute("hidden");
+    const existingIds = el.getAttribute("aria-describedby");
+    el.setAttribute("aria-describedby", existingIds + " " + errorId);
+  });
+  var triggers = document.querySelectorAll('.collapse [data-trigger="true"]');
+  triggers.forEach((el) => {
+    const tagName = el.tagName;
+    const dataAttributes = el.dataset;
+    let attrString = "";
+    Object.keys(dataAttributes).map((key) => {
+      if (key !== "trigger") {
+        attrString += ` data-${key} `;
+      }
+    });
+    el.innerHTML = `
+    <button aria-expanded="false" ${attrString}>${el.textContent}</button>`;
+    const getContent = (elem) => {
+      let elems = [];
+      while (elem.nextElementSibling && elem.nextElementSibling.tagName !== tagName) {
+        elems.push(elem.nextElementSibling);
+        elem = elem.nextElementSibling;
+      }
+      elems.forEach((node) => {
+        node.parentNode.removeChild(node);
+      });
+      return elems;
+    };
+    let contents = getContent(el);
+    let wrapper = document.createElement("div");
+    wrapper.hidden = true;
+    wrapper.setAttribute("class", "flow accessibility-menu");
+    contents.forEach((node) => {
+      wrapper.appendChild(node);
+    });
+    el.parentNode.insertBefore(wrapper, el.nextElementSibling);
+    let btn = el.querySelector("button");
+    btn.onclick = () => {
+      let expanded = btn.getAttribute("aria-expanded") === "true";
+      btn.setAttribute("aria-expanded", !expanded);
+      wrapper.hidden = expanded;
+    };
+  });
+  function setThemeFromLocalStorage() {
+    const value = localStorage.getItem("theme");
+    if (value == "light") {
+      document.documentElement.setAttribute("theme", "light");
+      localStorage.setItem("theme", "light");
+    }
+    if (value == "dark") {
+      document.documentElement.setAttribute("theme", "dark");
+      localStorage.setItem("theme", "dark");
+    }
+    const size = localStorage.getItem("fontsize");
+    let root = document.querySelector("html"), style = window.getComputedStyle(root, null).getPropertyValue("font-size"), fontSize = parseFloat(style);
+    root.style.fontSize = size;
+    const font = localStorage.getItem("fontstyle");
+    const html = document.querySelector("html");
+    html.setAttribute("font-style", font);
+  }
+  (function() {
+    setThemeFromLocalStorage();
+  })();
+})();
+//# sourceMappingURL=index.js.map
